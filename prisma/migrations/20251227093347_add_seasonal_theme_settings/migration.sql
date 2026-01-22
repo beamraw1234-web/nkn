6d821d8747e0c9f6abade8a1e7e28134e4c8759b
nikannlaya <nikannlaya.ph66@snru.ac.th>
2026-01-22 17:25:11 +0700

    git log --oneline --since="1 day ago"

--- prisma/migrations/20251227093347_add_seasonal_theme_settings/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `seasonalthemesettings` ADD COLUMN `effectColor` VARCHAR(191) NOT NULL DEFAULT '#ffffff',
-    ADD COLUMN `effectEnabled` BOOLEAN NOT NULL DEFAULT true,
-    ADD COLUMN `effectIntensity` DOUBLE NOT NULL DEFAULT 1.0,
-    ADD COLUMN `effectMaxParticles` INTEGER NOT NULL DEFAULT 100,
-    ADD COLUMN `effectSpeed` DOUBLE NOT NULL DEFAULT 1.0;
